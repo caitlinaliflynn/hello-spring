b4bd325c0f6dd1228576c705123969960fb7e50b
Ali <caitlinaliflynn@gmail.com>
2023-04-30 16:03:21 -0500

    end of exercises 13.5

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,79 +1,80 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
-//    @GetMapping("hello")
-//    @ResponseBody
-//    public String hello() {
-//
-//        return "Hello, Spring!";
-//    }
-
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String theGreeting = "Hello" + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
 
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    @GetMapping("hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model) {
+        String theGreeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", theGreeting);
+        return "hello";
     }
 
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
-    @RequestMapping(value="hello", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
 
-        return createMessage(name, language);
 
-    }
 
-    public static String createMessage(String n, String l) {
-        String greeting = "";
 
-        if (l.equals("english")) {
-            greeting = "Hello";
-        }
-        else if (l.equals("french")) {
-            greeting = "Bonjour";
-        }
-        else if (l.equals("italian")) {
-            greeting = "Bonjourno";
-        }
-        else if (l.equals("spanish")) {
-            greeting = "Hola";
-        }
-        else if (l.equals("german")) {
-            greeting = "Hallo";
-        }
 
-        return greeting + " " + n;
-    }
+//    @RequestMapping(value="hello", method = RequestMethod.POST)
+//    public String helloPost(@RequestParam String name, @RequestParam String language) {
+//        if (name == null) {
+//            name = "World";
+//        }
+//
+//        return createMessage(name, language);
+//
+//    }
+
+//    public static String createMessage(String n, String l) {
+//        String greeting = "";
+//
+//        if (l.equals("english")) {
+//            greeting = "Hello";
+//        }
+//        else if (l.equals("french")) {
+//            greeting = "Bonjour";
+//        }
+//        else if (l.equals("italian")) {
+//            greeting = "Bonjourno";
+//        }
+//        else if (l.equals("spanish")) {
+//            greeting = "Hola";
+//        }
+//        else if (l.equals("german")) {
+//            greeting = "Hallo";
+//        }
+//
+//        return greeting + " " + n;
+//    }
 
 
 }
