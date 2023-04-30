7845454b64de8eca65caaadc812d27efde516781
Ali <caitlinaliflynn@gmail.com>
2023-04-30 14:29:42 -0500

    chapter 10 code along

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -42,5 +42,38 @@ public class HelloController {
                 "</html>";
     }
 
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+
+        return greeting + " " + n;
+    }
+
 
 }
